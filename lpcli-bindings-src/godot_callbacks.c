b980428d4820f9e1adb63677748618a0b741b258
Cameron Reikes <cameronreikes@gmail.com>
2019-08-02 17:39:13 -0700

    Finish usefull bindings

--- lpcli-bindings-src/godot_callbacks.c
+++ lpcli-bindings-src/godot_callbacks.c
@@ -1,5 +1,7 @@
 #include <gdnative_api_struct.gen.h>
 
+#define LPCLI_ERROR(what) api->godot_print_error(what, __func__, __FILE__, __LINE__)
+
 #define LP_STATIC
 #define LP_IMPLEMENTATION
 #define PBKDF2_SHA256_STATIC
@@ -24,7 +26,7 @@ godot_variant LessPass_get_data(godot_object *p_instance, void *p_method_data,
                                 void *p_user_data, int p_num_args, godot_variant **p_args);
 
 godot_variant LessPass_get_error(godot_object *p_instance, void *p_method_data,
-                                void *p_user_data, int p_num_args, godot_variant **p_args);
+                                 void *p_user_data, int p_num_args, godot_variant **p_args);
 
 // called on library load by godot
 void GDN_EXPORT godot_gdnative_init(godot_gdnative_init_options *p_options)
@@ -73,7 +75,7 @@ void GDN_EXPORT godot_nativescript_init(void *p_handle)
     godot_instance_method get_error = {NULL, NULL, NULL};
     get_error.method = &LessPass_get_error;
     attributes.rpc_type = GODOT_METHOD_RPC_MODE_DISABLED;
-    nativescript_api->godot_nativescript_register_method(p_handle, "LessPass", "get_error", attributes,get_error);
+    nativescript_api->godot_nativescript_register_method(p_handle, "LessPass", "get_error", attributes, get_error);
 }
 
 typedef enum
@@ -124,6 +126,15 @@ godot_variant LessPass_get_error(godot_object *p_instance, void *p_method_data, 
 godot_variant LessPass_get_data(godot_object *p_instance, void *p_method_data,
                                 void *p_user_data, int p_num_args, godot_variant **p_args)
 {
+    godot_variant error_return;
+    api->godot_variant_new_int(&error_return, 1);
+
+    if (p_num_args != 3)
+    {
+        LPCLI_ERROR("Incorrect number of arguments. Arguments are [site],[login],[password]");
+        return error_return;
+    }
+
     // everything must use variants
     godot_string data;
     godot_variant ret;
@@ -132,11 +143,27 @@ godot_variant LessPass_get_data(godot_object *p_instance, void *p_method_data,
     LP_CTX ctx;
     LP_CTX_init(&ctx);
 
-    int gen_ret = LP_generate(&ctx, "test", "testing", "password");
+    godot_string site_name = api->godot_variant_as_string(p_args[0]);
+    godot_string user_name = api->godot_variant_as_string(p_args[1]);
+    godot_string password = api->godot_variant_as_string(p_args[2]); // TODO make more secure somehow
+
+    godot_char_string site_name_cs = api->godot_string_utf8(&site_name);
+    godot_char_string user_name_cs = api->godot_string_utf8(&user_name);
+    godot_char_string password_cs = api->godot_string_utf8(&password);
+
+    const char *site_name_c = api->godot_char_string_get_data(&site_name_cs);
+    const char *user_name_c = api->godot_char_string_get_data(&user_name_cs);
+    const char *password_c = api->godot_char_string_get_data(&password_cs);
+
+    int gen_ret = LP_generate(&ctx, site_name_c, user_name_c, password_c);
     if (gen_ret < 1)
     {
+        LPCLI_ERROR("Zero length output password");
         user_data->err = GODOT_LPCLI_ZERO_LENGTH;
-    } else {
+        return error_return;
+    }
+    else
+    {
         user_data->err = GODOT_LPCLI_OK;
     }
 
@@ -144,6 +171,9 @@ godot_variant LessPass_get_data(godot_object *p_instance, void *p_method_data,
     api->godot_string_parse_utf8(&data, ctx.buffer);
     api->godot_variant_new_string(&ret, &data);
     api->godot_string_destroy(&data);
+    api->godot_string_destroy(&site_name);
+    api->godot_string_destroy(&user_name);
+    api->godot_string_destroy(&password);
 
     return ret;
 }
--- src/Main.gd
+++ src/Main.gd
@@ -3,4 +3,10 @@ extends Node2D
 onready var lesspass_data = preload("res://bin/lesspass.gdns").new()
 
 func _ready():
-	print(lesspass_data.get_data())
+	var result = lesspass_data.get_data("test", "testing", "password")
+	if typeof(result) == TYPE_INT:
+		printerr("GDNATIVE BINDINGS ERROR")
+	elif typeof(result) == TYPE_STRING:
+		print(result)
+	else:
+		printerr("UNKNOWN ERROR")
