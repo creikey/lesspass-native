ed0326f36f94c44c4edc24b0d4cb887cbdeec592
Cameron Reikes <cameronreikes@gmail.com>
2019-08-06 15:44:25 -0700

    Add copy to clipboard button

--- src/UI.gd
+++ src/UI.gd
@@ -4,6 +4,11 @@ onready var lesspass_data = preload("res://bin/lesspass.gdns").new()
 
 func _on_GenerateButton_pressed():
 	var main_vbox_container = $PanelContainer/MarginContainer/MainVBoxContainer
+	var generate_button = main_vbox_container.get_node("PasswordPanel/PasswordHBox/GenerateButton")
+	var password_label = main_vbox_container.get_node("PasswordPanel/PasswordHBox/PasswordLabel")
+	if generate_button.text == "":
+		OS.clipboard = password_label.text
+		return
 	var site_name = main_vbox_container.get_node("SiteName").text
 	var user_name = main_vbox_container.get_node("UserName").text
 	var password = main_vbox_container.get_node("MasterPassword").text
@@ -13,9 +18,12 @@ func _on_GenerateButton_pressed():
 	if typeof(result) == TYPE_INT:
 		printerr("GDNATIVE BINDINGS ERROR")
 	elif typeof(result) == TYPE_STRING:
-		var password_label = main_vbox_container.get_node("PasswordPanel/PasswordHBox/PasswordLabel")
+
 		password_label.visible = true
 		print(result)
 		password_label.text = result
+
+		generate_button.text = ""
+		generate_button.icon = preload("res://copy-to-clipboard.svg")
 	else:
 		printerr("UNKNOWN ERROR")
